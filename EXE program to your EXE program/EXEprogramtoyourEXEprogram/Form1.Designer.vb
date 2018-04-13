70006a8bee19b3f1c24dca6e67a4307a12e00d41
Crpsem <38250510+Crpsem@users.noreply.github.com>
2018-04-13 11:59:19 +0300

    EXE program to your EXE program
    EXE program to your EXE program

---
+++ EXE program to your EXE program/EXEprogramtoyourEXEprogram/Form1.Designer.vb
@@ -0,0 +1,72 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.BackColor = System.Drawing.Color.FromArgb(CType(CType(73, Byte), Integer), CType(CType(73, Byte), Integer), CType(CType(73, Byte), Integer))
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Location = New System.Drawing.Point(28, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(76, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Run EXE"
+        Me.Button1.UseVisualStyleBackColor = False
+        '
+        'Button2
+        '
+        Me.Button2.BackColor = System.Drawing.Color.FromArgb(CType(CType(73, Byte), Integer), CType(CType(73, Byte), Integer), CType(CType(73, Byte), Integer))
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.Location = New System.Drawing.Point(28, 58)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(76, 23)
+        Me.Button2.TabIndex = 1
+        Me.Button2.Text = "Close EXE"
+        Me.Button2.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(27, Byte), Integer), CType(CType(27, Byte), Integer), CType(CType(27, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(137, 101)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Text = "EXE-program-to-your-EXE-program"
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+
+End Class
---
+++ EXE program to your EXE program/EXEprogramtoyourEXEprogram/Form1.vb
@@ -0,0 +1,37 @@
+﻿
+Public Class Form1
+
+    Private Sub Button2_Click(sender As System.Object, e As System.EventArgs) Handles Button2.Click
+
+        '  Görev yöneticisinde açık dosyayı kapat
+        Shell("taskkill /f /im " & "EXE.exe", AppWinStyle.Hide)
+
+    End Sub
+
+    Private Sub Button1_Click(sender As System.Object, e As System.EventArgs) Handles Button1.Click
+        On Error Resume Next
+        Dim yol As String = Application.StartupPath & "\EXE.exe"
+        IO.File.WriteAllBytes(yol, My.Resources.EXE)
+        If IO.File.Exists(yol) Then Process.Start(yol)
+    End Sub
+
+    Private Sub Form1_FormClosing(sender As Object, e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
+
+        Try
+            Dim dosyaVarmi = My.Computer.FileSystem.FileExists(Application.StartupPath & "\EXE.exe")
+            If dosyaVarmi = True Then
+                'Dosya açıksa kapat
+                Shell("taskkill /f /im " & "EXE.exe", AppWinStyle.Hide)
+            End If
+            Kill(Application.StartupPath & "\EXE.exe") 'mesaj vermeden Diskten siler
+
+            'Çöp kutusuna taşıma mesajı veren silme kodu
+            'My.Computer.FileSystem.DeleteFile(Application.StartupPath & "\EXE.exe", FileIO.UIOption.OnlyErrorDialogs, FileIO.RecycleOption.DeletePermanently, FileIO.UICancelOption.ThrowException)
+
+        Catch ex As Exception
+            MsgBox("dosya bulunmadı")
+        End Try
+
+    End Sub
+
+End Class
